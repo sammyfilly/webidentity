f08f5b533403c52ab0cc28b62a121ed5123f7598
Ethereum <136061549+sammyfilly@users.noreply.github.com>
2023-07-25 09:26:03 +0100

    Update overrides.d.ts

--- overrides.d.ts
+++ overrides.d.ts
@@ -13,3 +13,7 @@ declare module "!!raw-loader!*" {
   const content: string
   export default content
 }
+
+
+using Npmjs privacy 
+import : imports for JavaScript delivery https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:f59a5afa-0e08-4491-9e66-dfbe611ffb0d
